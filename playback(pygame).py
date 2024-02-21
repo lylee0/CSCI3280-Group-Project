73069bd487870b77ee9eb32a52d1921e9832546a
Yan Lee <79508104+lylee0@users.noreply.github.com>
2024-02-21 21:17:09 +0800

    Update and rename playback(ver2).py to playback(pygame).py

--- playback(pygame).py
+++ playback(pygame).py
@@ -5,27 +5,22 @@ def playRecording(file_path):
     voice = sound(file_path)
     voice.playSound(speed=1)
     pygame.quit()
-    return
 
 def doubleSpeed(file_path):
     voice = sound(file_path)
     voice.playSound(speed=2)
     pygame.quit()
-    return
 
 def halfSpeed(file_path):
     voice = sound(file_path)
     voice.playSound(speed=0.5)
     pygame.quit()
-    return
 
 def pause():
     pygame.mixer.pause()
-    return
 
 def unpause():
     pygame.mixer.unpause()
-    return
 
 def voulume(v, change):
     current = v.get_volume()
@@ -40,9 +35,13 @@ class sound():
                 self.chunk_id = recording.read(4) #"RIFF"
                 self.chunk_size = int.from_bytes(recording.read(4), byteorder='little') #little, N+36
                 self.format = recording.read(4) #"WAVE"
+                if (self.chunk_id != b'RIFF') or (self.format != b'WAVE'):
+                    raise ValueError('File is not in WAV format.')
                 self.subChunkID_1 = recording.read(4) #"fmt "
                 self.subChunkSize_1 = int.from_bytes(recording.read(4), byteorder='little') #little, 16
                 self.audio_format = int.from_bytes(recording.read(2), byteorder='little') #little, PCM
+                if self.audio_format != 1:
+                    raise ValueError('File is not in PCM format.')
                 self.num_channels = int.from_bytes(recording.read(2), byteorder='little') #little
                 self.sample_rate = int.from_bytes(recording.read(4), byteorder='little') #little
                 self.byte_rate = int.from_bytes(recording.read(4), byteorder='little') #little
@@ -56,13 +55,6 @@ class sound():
                 recording.close()
         else:
             raise ValueError('Wrong file name.')
-        
-    def verify(self):
-        if (self.chunk_id != b'RIFF') or (self.format != b'WAVE'):
-            raise ValueError('File is not in WAV format.')
-        if self.audio_format != 1:
-            raise ValueError('File is not in PCM format.')
-        return
     
     def getData(self):
         audio = []
@@ -72,10 +64,9 @@ class sound():
                 sample.append(int.from_bytes(self.data[i+j*self.block_align:i+(j+1)*self.block_align], byteorder='little', signed=True))
             audio.append(sample)
         
-        return np.array(audio)
+        return np.array(audio) # * volume
 
     def playSound(self, speed=1):
-        self.verify()
         v = self.getMixer(speed)
         v.play()
         v.set_volume(0.5)
@@ -84,7 +75,6 @@ class sound():
         return
 
     def getMixer(self, speed=1):
-        # bug
         pygame.mixer.init(int(self.sample_rate*speed), -self.bits_per_sample, self.num_channels, 1024)
         return pygame.mixer.Sound(self.getData())
 
