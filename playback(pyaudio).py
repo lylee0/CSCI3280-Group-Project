598b9a234c3672acbcff3bf4e436fbc3066e9c4b
Yan Lee <79508104+lylee0@users.noreply.github.com>
2024-02-21 21:36:06 +0800

    Update and rename playback(pygame).py to playback(pyaudio).py

--- playback(pyaudio).py
+++ playback(pyaudio).py
@@ -1,35 +1,22 @@
 import numpy as np
-import pygame
+import pyaudio
 
-def playRecording(file_path):
-    voice = sound(file_path)
-    voice.playSound(speed=1)
-    pygame.quit()
+def playRecording(file_path, speed=1):
+    voice = sound(file_path, speed)
+    voice.playSound()
 
-def doubleSpeed(file_path):
-    voice = sound(file_path)
-    voice.playSound(speed=2)
-    pygame.quit()
+def doubleSpeed(file_path, speed=2):
+    voice = sound(file_path, speed)
+    voice.playSound()
 
-def halfSpeed(file_path):
-    voice = sound(file_path)
-    voice.playSound(speed=0.5)
-    pygame.quit()
-
-def pause():
-    pygame.mixer.pause()
-
-def unpause():
-    pygame.mixer.unpause()
-
-def voulume(v, change):
-    current = v.get_volume()
-    v.set_volume(current + change)
-    return
+def halfSpeed(file_path, speed=0.5):
+    voice = sound(file_path, speed)
+    voice.playSound()
 
 class sound():
-    def __init__(self, file_path):
+    def __init__(self, file_path, speed=1):
         self.file_path = file_path
+        self.speed = speed
         if file_path[-4:] == ".wav":
             with open(self.file_path, "rb") as recording:
                 self.chunk_id = recording.read(4) #"RIFF"
@@ -64,19 +51,18 @@ class sound():
                 sample.append(int.from_bytes(self.data[i+j*self.block_align:i+(j+1)*self.block_align], byteorder='little', signed=True))
             audio.append(sample)
         
-        return np.array(audio) # * volume
+        return np.array(audio, dtype=np.int32) 
 
-    def playSound(self, speed=1):
-        v = self.getMixer(speed)
-        v.play()
-        v.set_volume(0.5)
-        while(pygame.mixer.get_busy()):
-            continue
-        return
-
-    def getMixer(self, speed=1):
-        pygame.mixer.init(int(self.sample_rate*speed), -self.bits_per_sample, self.num_channels, 1024)
-        return pygame.mixer.Sound(self.getData())
+    def playSound(self):
+        p = pyaudio.PyAudio()
+        self.stream = p.open(format=pyaudio.paInt32,
+                channels=self.num_channels,
+                rate=int(self.sample_rate * self.speed),
+                output=True)
+        self.stream.write(self.data)
+        self.stream.stop_stream()
+        self.stream.close()
+        p.terminate()
 
 if __name__ == "__main__":
     playRecording("test.wav")
