52c0ccdbcfa1d970f4a0edcbd7cc482876d5bcf0
develop555ben <develop555ben@gmail.com>
2024-02-22 00:37:04 +0800

    Windows Form UI update

---
+++ CSCI3280_GP_Project/CSCI3280_GP_Project/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSCI3280_GP_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < 100; i++) 
+            {
+                Button button = new Button();
+                button.Text = i.ToString();
+                button.MouseDown += new MouseEventHandler(this.ButtonClick);
+
+                recordingContents.Controls.Add(button);
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void recordingContents_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void recordingButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void playButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void ButtonClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                timeDetails.Text = "Left";
+            }
+            if (e.Button == MouseButtons.Right)
+            {
+                timeDetails.Text = "Right";
+            }
+        }
+    }
+}
---
+++ CSCI3280_GP_Project/CSCI3280_GP_Project/FunctionCalling.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSCI3280_GP_Project
+{
+    static class FunctionCalling
+    {
+        public static void CallPythonFunction(string arg = "")
+        {
+            string path = "python";
+
+            //string arg = Application.persistentDataPath + $"/try.py";
+
+            // Create a process
+            Process process = new Process();
+
+            // Set the StartInfo of process
+            process.StartInfo.FileName = path;
+            process.StartInfo.Arguments = arg;
+
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+
+            process.StartInfo.CreateNoWindow = true;
+            //process.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
+
+            // Start the process
+            process.Start();
+
+            string consoleMessage = process.StandardOutput.ReadToEnd();
+            string errorMessage = process.StandardError.ReadToEnd();
+            process.WaitForExit();
+        }
+    }
+}
