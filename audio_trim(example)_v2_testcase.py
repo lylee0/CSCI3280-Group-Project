3b6e94f945a0ee00789b5e9fadc47922d88cf959
TLR6325 <114717667+TLR6325@users.noreply.github.com>
2024-02-26 02:53:03 +0800

    Seperate test case coding & propose update coding

--- audio_trim(example)_v2_testcase.py
+++ audio_trim(example)_v2_testcase.py
@@ -1,11 +1,11 @@
-import playback
+import playbackk
 import soundRecording
 import numpy as np
-#import time
+import time
 
 def edit(file_path, start, end, speed, volume):
     # return frames to write
-    wav = playback.getData(file_path)
+    wav = playbackk.getData(file_path)
     sample_rate = wav["sample_rate"]
     if speed == 1:
         dataNormal = wav["dataNormal"]
@@ -25,8 +25,8 @@ def edit(file_path, start, end, speed, volume):
     else:
         raise ValueError('Invalid speed.')
 
-def overwrite(file_path, start_record, end_record, frames):
-    wav = playback.getData(file_path)
+def overwrite(file_path, start_record, end_record, deviceIndex, overwriteTime):
+    wav = playbackk.getData(file_path)
     sample_rate = wav["sample_rate"]
     block_align = wav["block_align"]
     dataNormal = wav["dataNormal"]
@@ -34,13 +34,13 @@ def overwrite(file_path, start_record, end_record, frames):
     bits_per_sample = wav["bits_per_sample"]
     dataNormal_1 = dataNormal[:int(start_record * sample_rate)]
     dataNormal_3 = dataNormal[int(end_record * sample_rate):]
-    '''# call startRecording
-    streamObj, pObj = soundRecording.startRecording(44100, 1024, 2, 1)
+    # call startRecording
+    streamObj, pObj = soundRecording.startRecording(44100, 1024, num_channels, deviceIndex)
     # call threadWriting, get frames
     frames = soundRecording.threadWriting(streamObj, 1024)
-    time.sleep(1)
+    time.sleep(overwriteTime)
     # call stopRecording
-    soundRecording.stopRecording(streamObj, pObj)'''
+    soundRecording.stopRecording(streamObj, pObj)
     temp = b''.join(frames)
     frames=temp
     audio_normal = []
@@ -49,17 +49,19 @@ def overwrite(file_path, start_record, end_record, frames):
         for j in range(num_channels):
             sample.append(int.from_bytes(frames[i+j*4:i+(j+1)*4], byteorder='little', signed=True)) #Force 32-bit by recording mechanism
         audio_normal.append(sample)
+    print(audio_normal)
     dataNormal_2 = np.array(audio_normal, dtype=np.int32) 
     dataArray = np.concatenate((dataNormal_1, dataNormal_2), axis=0)
     dataArray = np.concatenate((dataArray, dataNormal_3), axis=0)
+    print(dataArray)
     return [dataArray.astype(np.int32).tobytes()]
 
 if __name__ == "__main__":
     # call record, get frames
     # call overwrite to concatenate
-    frames = overwrite("example.wav", 1, 2, new_record)
+    frames = overwrite("test.wav", 1, 2, 1)
     # call fileWriting to write file
-    soundRecording.fileWriting(frames, 2, 44100, 'example_edit.wav')
+    soundRecording.fileWriting(frames, 2, 44100, 'example_overwrite.wav')
 
     frames = edit("test.wav", 0, 1, 1, 2)
     soundRecording.fileWriting(frames, 2, 44100, 'example_edit.wav')
