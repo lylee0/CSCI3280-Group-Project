725b0d88607bc5e5dd0eee5b4fab554df4ccd557
Yan Lee <79508104+lylee0@users.noreply.github.com>
2024-03-24 19:18:00 +0800

    Update record_server.py

--- Phase 2/functions/record_server.py
+++ Phase 2/functions/record_server.py
@@ -5,10 +5,7 @@ import numpy as np
 import cv2
 import pyautogui
 import pygetwindow as gw
-import threading
-import time
-import datetime
-from pydub import AudioSegment
+import socket
 
 
 clients = set() # a list?
@@ -20,15 +17,20 @@ channel = 1
 samp_width = 2
 fs = 44100
 block_align = 2 #???
+PORT = 8765
+IP = socket.gethostbyname('localhost')
+ADDR = (IP, PORT)
+CONNECTIONS = set()
 
 
 '''
     Recieve audio from all clients
 '''
 async def receive_audio(websocket, client_id):
+    # can client_id be 0 t0 len(clients)?
     global clients
 
-    async for message in websocket:
+    async for message in websocket: # change 
         if recording:
             audio_bytes[client_id].append(message)
 
@@ -53,8 +55,12 @@ async def byte_to_int():
     global audio_bytes, audio_array
     # length of audio_bytes == number of users
     while audio_bytes:
-        for bytes in audio_bytes:
-            pass
+        #for bytes in audio_bytes:
+        for i in range(len(audio_bytes)):
+            # assume 16 bits
+            data = int.from_bytes(audio_bytes[i][:2], byteorder='little', signed=True)
+            audio_bytes[i] = audio_bytes[i][2:]
+            audio_array[i].append(data)
 
 '''
     Send merged audio data to all clients
@@ -70,6 +76,7 @@ async def send_audio(websocket):
 async def handle_server(websocket): # client connects to server
     global clients, recording, audio_bytes, audio_array, audio_merged
 
+    clients.add(websocket)
     #client_id = id(websocket)
 
     async for message in websocket:
@@ -79,9 +86,12 @@ async def handle_server(websocket): # client connects to server
             audio_array = [] # 2d numpy array
             audio_merged = []
 
+            # send to all clients
             await websocket.send('Start Recording')
 
-            task = asyncio.create_task(receive_audio(websocket, client_id)) # recieve audio from clients
+            client = clients[0]
+            # change this ???
+            task = asyncio.create_task(receive_audio(websocket, client.index())) # recieve audio from clients
             # convert audio to int
             await byte_to_int() # need await???
             # merge users audio
@@ -103,5 +113,9 @@ async def handle_server(websocket): # client connects to server
             # send message when all required data is sent to clients???
             await websocket.send('Stop Recording')
 
+async def main():
+    async with websockets.serve(handle_server, "localhost", PORT):
+        await asyncio.Future()
+
 if __name__ == "_main_":
-    asyncio.run(handle_server(websocket))
+    asyncio.run(main())
