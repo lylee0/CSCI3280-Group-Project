71a52c5e5705769f37b95be6a770afc30e32d288
Yan Lee <79508104+lylee0@users.noreply.github.com>
2024-03-26 02:45:04 +0800

    Update chat_client.py

--- chat_client.py
+++ chat_client.py
@@ -3,6 +3,7 @@ import asyncio
 import websockets
 import sys
 import pyaudio
+import threading
 
 PORT = "8765"
 HOST = "localhost"
@@ -15,13 +16,17 @@ CHUNK = 1024
 
 audio = pyaudio.PyAudio()
 stream = audio.open(format=FORMAT, channels=CHANNEL, rate=RATE, input=True, frames_per_buffer=CHUNK, input_device_index = 1)
+stream_play = audio.open(format=FORMAT, channels=CHANNEL, rate=RATE, output=True, frames_per_buffer=CHUNK)
+
+audio_record = []
+audio_receive = []
 
 def unmute():
     global audio
     global stream
 
     audio = pyaudio.PyAudio()
-    stream = audio.open(format=FORMAT, channels=CHANNEL, rate=RATE, input=True, frames_per_buffer=CHUNK, input_device_index = 1)
+    stream = audio.open(format=FORMAT, channels=CHANNEL, rate=RATE, input=True, frames_per_buffer=CHUNK, input_device_index = 1) #, input_device_index = 1
 
 def mute():
     global audio
@@ -31,36 +36,44 @@ def mute():
     stream.close()
     audio.terminate()
 
-async def send_audio():
-    async with websockets.connect(URI) as websocket:
-        while stream.is_active():
-            while True:
-                data = stream.read(CHUNK)
-                if data[:1] != b'\x00':
-                    await websocket.send(data)
+def record_pc():
+    while stream.is_active():
+        data = stream.read(CHUNK)
+        audio_record.append(data)
 
-async def play_audio():
-    stream_play = audio.open(format=FORMAT, channels=CHANNEL, rate=RATE, output=True, frames_per_buffer=CHUNK)
-    async with websockets.connect(URI) as websocket:
-        while True:
+async def send_audio(websocket):
+    while True:
+        if audio_record:
+            await websocket.send(audio_record.pop(0))
+        else:
+            continue
+
+async def receive_audio(websocket):
+    while True:
+        async for data in websocket:
+            audio_receive.append(data)
+
+def play_audio():
+    while True:
+        if audio_receive:
+            print(audio_receive)
+            stream_play.write(audio_receive.pop(0))
+            
             #data = await websocket.recv()
-            async for data in websocket:
-                if data:
-                    #print(data)
-                    await play(stream_play, data)
+        else:
+            continue
 
-async def play(stream_play, data):
-    #print(data[:1])
-    if data[:1] != b'\x00':
-        #print(data)
-        stream_play.write(data)
 
 async def main():
-    task_send_audio = asyncio.create_task(send_audio())
-    task_play_audio = asyncio.create_task(play_audio())
-
-    # Wait for either task to complete
-    await asyncio.gather(task_send_audio, task_play_audio)
+    async with websockets.connect(URI) as websocket:
+        record_thread = threading.Thread(target=record_pc)
+        play_thread = threading.Thread(target=play_audio)
+        task_send_audio = asyncio.create_task(send_audio(websocket))
+        task_receive_audio = asyncio.create_task(receive_audio(websocket))
+        record_thread.start()
+        play_thread.start()
+        # Wait for either task to complete
+        await asyncio.gather(task_send_audio, task_receive_audio)
 
     # close audio stream when user leave chat room
     mute()
