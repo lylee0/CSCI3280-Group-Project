8f01f16c40143853460a3e8cc61298016dbf4663
Yan Lee <79508104+lylee0@users.noreply.github.com>
2024-03-27 00:47:43 +0800

    Update socket_client.py

--- Phase 2/voice_chat/socket_client.py
+++ Phase 2/voice_chat/socket_client.py
@@ -21,8 +21,9 @@ audio = pyaudio.PyAudio()
 stream = audio.open(format=FORMAT, channels=CHANNEL, rate=RATE, input=True, frames_per_buffer=CHUNK, input_device_index=1)
 stream_play = audio.open(format=FORMAT, channels=CHANNEL, rate=RATE, output=True, frames_per_buffer=CHUNK)
 
-audio_record = b''
-audio_receive = b''
+audio_dict = {}
+audio_record = {}
+#audio_receive = b''
 
 def unmute():
     global audio
@@ -39,42 +40,64 @@ def mute():
     stream.close()
     audio.terminate()
 
-def record_pc():
-    global audio_record
+
+'''
+    Record client audio from device (only one client)
+'''
+def record_pc(client_socket):
+    global audio_dict, audio_record
     while stream.is_active():
         data = stream.read(CHUNK)
-        audio_record += data
+        if recording == False:
+            audio_dict[client_socket].append(data)
+        else:
+            audio_dict[client_socket].append(data)
+            audio_record[client_socket].append(data)
 
+'''
+    Send client audio to server (only one client)
+'''
 def send_audio(client_socket):
-    global audio_record
+    global audio_dict
     while True:
-        if audio_record:
-            data = audio_record[:CHUNK]
-            audio_record = audio_record[CHUNK:]
+        if audio_dict[client_socket]:
+            data = audio_dict[client_socket].pop(0)
             client_socket.sendall(data)
         else:
             continue
 
+'''
+    Need to receive multiple users audio at the same time (more than one client)
+'''
 def receive_audio(client_socket):
-    global audio_receive
+    global audio_dict, audio_record
     while True:
         data = client_socket.recv(CHUNK)
-        print(data)
+        #print(data)
         if not data:
             continue
         else:
-            audio_receive += data
+            if recording == False:
+                audio_dict[client_socket].append(data)
+            else:
+                audio_dict[client_socket].append(data)
+                audio_record[client_socket].append(data)
 
-def play_audio():
-    global audio_receive
+'''
+    Need to play multiple users audio at the same time (more than one client)
+'''
+def play_audio(client_socket):
+    global audio_dict
     while True:
-        if len(audio_receive) >= CHUNK:
-            data = audio_receive[:CHUNK]
-            audio_receive = audio_receive[CHUNK:]
+        if audio_dict[client_socket]:
+            data = audio_dict[client_socket].pop(0)
             stream_play.write(data)
         else:
             continue
 
+'''
+    Check how many clients
+'''
 def handle_client(client_socket):
     global clients
     with clients_lock:
@@ -82,6 +105,7 @@ def handle_client(client_socket):
         print(f"Connected clients: {len(clients)}")
 
     receive_audio(client_socket)
+    play_audio(client_socket) # not sure
 
     with clients_lock:
         clients.remove(client_socket)
@@ -90,25 +114,28 @@ def handle_client(client_socket):
     client_socket.close()
 
 def main():
-    global audio_receive, audio_record
+    global audio_dict
 
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((HOST, int(PORT)))
 
-    record_thread = threading.Thread(target=record_pc)
-    play_thread = threading.Thread(target=play_audio)
+    audio_dict[client_socket] = []
+    audio_record[client_socket] = []
+
+    record_thread = threading.Thread(target=record_pc, args=(client_socket,))
+    #play_thread = threading.Thread(target=play_audio)
     task_send_audio = threading.Thread(target=send_audio, args=(client_socket,))
     # need to handle data from different client, better set a dictionary with client as the key, and values to be the buffer
     client_thread = threading.Thread(target=handle_client, args=(client_socket,))
 
     record_thread.start()
-    play_thread.start()
+    #play_thread.start()
     task_send_audio.start()
     client_thread.start()
 
     try:
         record_thread.join()
-        play_thread.join()
+        #play_thread.join()
         task_send_audio.join()
         client_thread.join()
     except KeyboardInterrupt:
