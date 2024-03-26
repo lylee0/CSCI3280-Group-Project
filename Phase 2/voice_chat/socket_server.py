8e98d139ee99eb7dfd4c83f84dfb78ac326efdc4
Yan Lee <79508104+lylee0@users.noreply.github.com>
2024-03-27 00:12:45 +0800

    Create socket_server.py

---
+++ Phase 2/voice_chat/socket_server.py
@@ -0,0 +1,61 @@
+import socket
+import threading
+import pyaudio
+
+PORT = 8765
+IP = socket.gethostbyname('localhost')
+ADDR = (IP, PORT)
+CONNECTIONS = set()
+
+FORMAT = pyaudio.paInt16
+CHANNEL = 1
+RATE = 8000
+CHUNK = 1024
+audio_receive = b'' # only store 1 client?
+
+def handle_connection(client_socket, address):
+    global audio_receive
+    CONNECTIONS.add(client_socket)
+    audio_receive = b''
+    print(f'connected client: {address}')
+    receive_thread = threading.Thread(target=receive_audio, args=(client_socket,))
+    broadcast_thread = threading.Thread(target=broadcast, args=(client_socket,))
+    receive_thread.start()
+    broadcast_thread.start()
+    #receive_audio(client_socket)
+
+def receive_audio(client_socket):
+    global audio_receive
+    while True:
+        data = client_socket.recv(CHUNK)
+        audio_receive += data
+        #print(data)
+
+def broadcast(sender):
+    global audio_receive
+    while True:
+        if audio_receive:
+            data = audio_receive[:CHUNK]
+            audio_receive = audio_receive[CHUNK:]
+            for client_socket in CONNECTIONS:
+                if client_socket != sender:
+                    client_socket.sendall(data)
+                #print(data)
+        else:
+            continue
+
+def main():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(ADDR)
+    server_socket.listen()
+
+    print(f'Serving on {ADDR}')
+
+    while True:
+        client_socket, address = server_socket.accept()
+        client_thread = threading.Thread(target=handle_connection, args=(client_socket, address))
+        client_thread.start()
+
+if __name__ == '__main__':
+    print(f'The server has started running')
+    main()
