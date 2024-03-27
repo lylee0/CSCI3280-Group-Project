3026b3b337b83baac2cd17cde5eda5fff5f448c0
Yan Lee <79508104+lylee0@users.noreply.github.com>
2024-03-27 12:57:33 +0800

    Update socket_client.py

--- Phase 2/voice_chat/socket_client.py
+++ Phase 2/voice_chat/socket_client.py
@@ -1,10 +1,10 @@
 import socket
-import struct
 import os
 import pyaudio
 import threading
 import datetime
 import wave
+from pydub import AudioSegment
 
 PORT = "8765"
 HOST = "localhost"
@@ -19,6 +19,7 @@ CHUNK = 1024
 sample_width = 2
 
 recording = False
+chatroom = True
 
 audio = pyaudio.PyAudio()
 stream_input = audio.open(format=FORMAT, channels=CHANNEL, rate=RATE, input=True, frames_per_buffer=CHUNK, input_device_index=1)
@@ -26,9 +27,8 @@ stream_output = audio.open(format=FORMAT, channels=CHANNEL, rate=RATE, output=Tr
 
 audio_receive = {}
 audio_record = {}
-audio_write = b''
 
-file_start_time = 1
+file_start_time = 0
 
 def unmute():
     global audio, stream_input
@@ -47,21 +47,29 @@ def mute():
     Record client audio from device (only one client)
 '''
 def record_pc(client_socket):
-    global audio_receive, audio_record
-    while stream_input.is_active():
-        data = stream_input.read(CHUNK)
-        if recording == False:
-            audio_receive[client_socket].append(data)
+    global chatroom, audio_receive, audio_record, sample_width
+    while chatroom:
+        if stream_input.is_active():
+            data = stream_input.read(CHUNK)
+            if recording == False:
+                audio_receive[client_socket].append(data)
+            else:
+                audio_receive[client_socket].append(data)
+                audio_record[client_socket].append(data)
         else:
-            audio_receive[client_socket].append(data)
-            audio_record[client_socket] += data
+            data = bytes([0] * sample_width * CHUNK)
+            if recording == False:
+                audio_receive[client_socket].append(data)
+            else:
+                audio_receive[client_socket].append(data)
+                audio_record[client_socket].append(data)
 
 '''
     Send client audio to server (only one client)
 '''
 def send_audio(client_socket):
     global audio_receive
-    while True:
+    while chatroom:
         if audio_receive[client_socket]:
             data = audio_receive[client_socket].pop(0)
             client_socket.sendall(data)
@@ -73,27 +81,38 @@ def send_audio(client_socket):
 '''
 def receive_audio(client_socket):
     global audio_receive, audio_record, file_start_time
-    while True:
+    while chatroom:
         data = client_socket.recv(CHUNK)
-        # if data == "Start Recording", record the time and save in file_start_time, recording == True
-        # file_start_time = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-        # if data == "Stop Recording", recording == False
-        #print(data)
+        check = 1
         if not data:
             continue
+
+        try:
+            check = data.decode('utf-8')
+        except:
+            pass
+
+        if check == "Start Recording":
+            recording = True
+            file_start_time = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+            merge_thread = threading.Thread(target=merge_write)
+            merge_thread.start()
+        elif check == "Stop Recording":
+            recording = False
+            audio_record[client_socket].append(data.decode('utf-8'))
         else:
             if recording == False:
                 audio_receive[client_socket].append(data)
             else:
                 audio_receive[client_socket].append(data)
-                audio_record[client_socket] += data
+                audio_record[client_socket].append(data)
 
 '''
     Need to play multiple users audio at the same time (more than one client)
 '''
 def play_audio(client_socket):
     global audio_receive, stream_output
-    while True:
+    while chatroom:
         if audio_receive[client_socket]:
             data = audio_receive[client_socket].pop(0)
             stream_output.write(data)
@@ -121,58 +140,48 @@ def handle_client(client_socket):
 '''
     Write the wav file header (all clients will write at the same time)
 '''
-def write_file_header(f):
-    global CHANNEL, RATE, sample_width
+def write_file_header():
+    global CHANNEL, RATE, sample_width, f
     # open file here
     f.setnchannels(CHANNEL)
     f.setsampwidth(sample_width) # 2 bytes
     f.setframerate(RATE)
 
 '''
-    Write wav file
+    Merge and write all users audio
 '''
-def write_file():
-    global audio_write, file_start_time
-    # only open the file and write the header if it does not exit, record the time received start recording
-    # can also add a path
+def merge_write():
+    global audio_record, sample_width, f
     output_file = f"recording_{file_start_time}.wav"
-    if not os.path.exists(output_file):
+    if (not os.path.exists(output_file)) and (file_start_time != 0):
         f = wave.open(output_file, 'wb')
-        write_file_header(f)
-    while True: # may need to change, maybe the last element in the list???
-        if audio_write:
-            data = audio_write[:CHUNK]
-            audio_write = audio_write[CHUNK:]
-            f.write(data)
-            # need to flush the data
-            # when to close the file?
-        else:
-            continue
-
-'''
-    Merge all users audio
-'''
-def merge_audio():
-    global audio_record, audio_write, sample_width
+        write_file_header()
     while True:
-        #all_audio = audio_record.values()
-        #all_audio = list(all_audio)
+        audio_zero = bytes([0] * sample_width * CHUNK)
+        audio_merge = AudioSegment(audio_zero,sample_width=2,channels=CHANNEL,frame_rate=RATE)
         all_clients = audio_record.keys()
-        #if all_audio:
-        compare = []
         for client in all_clients:  # not sure if need to change to 
-            audio_bytes = audio_record[client][:sample_width]
-            compare.append(struct.unpack('<h', audio_bytes)[0])
-            audio_record[client] = audio_record[client][sample_width:]
-        audio_write += max(compare)
-        #else: 
-            #continue
+            if audio_record[client] == []:
+                continue
+            audio_bytes = audio_record[client].pop(0)
+            try:
+                if audio_bytes.decode('utf-8') == "Stop Recording":
+                    f.flush()
+                    f.close()
+                    break
+            except:
+                pass
+            audio = AudioSegment(audio_bytes,sample_width=2,channels=CHANNEL,frame_rate=RATE)
+            audio_merge = audio.overlay(audio_merge)
+
+        print(audio_merge.raw_data)
+        f.writeframesraw(audio_merge.raw_data)
 
 def send_start(client_socket):
-    client_socket.sendall("Start Recording") # only send to server
+    client_socket.sendall("Start Recording".encode('utf-8')) # only send to server
 
 def send_stop(client_socket):
-    client_socket.sendall("Stop Recording") # only send to server
+    client_socket.sendall("Stop Recording".encode('utf-8')) # only send to server
 
 def main():
     global audio_receive, audio_record, audio_write
@@ -181,23 +190,21 @@ def main():
     client_socket.connect((HOST, int(PORT)))
 
     audio_receive[client_socket] = []
-    audio_record[client_socket] = b''
-    audio_write = b''
+    audio_record[client_socket] = []
+    #audio_write = b''
 
     record_thread = threading.Thread(target=record_pc, args=(client_socket,))
     #play_thread = threading.Thread(target=play_audio)
     task_send_audio = threading.Thread(target=send_audio, args=(client_socket,))
     # need to handle data from different client, better set a dictionary with client as the key, and values to be the buffer
     client_thread = threading.Thread(target=handle_client, args=(client_socket,))
-    merge_thread = threading.Thread(target=merge_audio)
-    write_thread = threading.Thread(target=write_file) # run the infinite loop???
+    #merge_thread = threading.Thread(target=merge_write)
 
     record_thread.start()
     #play_thread.start()
     task_send_audio.start()
     client_thread.start()
-    merge_thread.start()
-    write_thread.start()
+    #merge_thread.start()
 
     try:
         record_thread.join()
