a2a15060ca83d9cb5b5d219fd3f3f5d67e998383
Yan Lee <79508104+lylee0@users.noreply.github.com>
2024-04-01 17:49:45 +0800

    Update multiUserChatGui.py

--- Phase 2/multiUserChatGui.py
+++ Phase 2/multiUserChatGui.py
@@ -323,7 +323,6 @@ class MultiUserChatWindow(QWidget):
         self.timer.stop()
         self.online = False
         if recording:
-            recording = False
             audio_merge = self.merge()
             write_thread = threading.Thread(target=self.writeFile, args=(audio_merge,))
             write_thread.start()
